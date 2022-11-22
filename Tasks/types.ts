deb382a622a324fdb2840969144d53facfd39f33
isnaddinovotabek <isnaddinovotabek@gmail.com>
2022-11-22 19:19:08 +0500

    types bn ishlash

---
+++ Tasks/types.ts
@@ -0,0 +1,93 @@
+// type add = (a: string, b: number) => string
+// function add(a: string, b: number) {
+//     return a + b
+// }
+
+// console.log(add("Otabek ", 1))
+
+//------------------------------
+
+// type person = (firstname: string, lastname: string) => number
+// function person(firstname: string, lastname: string) {
+//     return 2
+// }
+
+// console.log(person("a", "qw"));
+
+//------------------------------------
+
+// type killProcess = (app: string) => string
+// function killProcess(app: string) {
+//     return "process killed"
+// }
+
+// console.log(killProcess("String"));
+
+//-------------------------------------
+
+// type clone = ()=> string
+// function clone() {
+//     return "cloden"
+// }
+
+// console.log(clone());
+
+//----------------------------------
+
+// type user ={name:string, surname:string, age:number}
+// const user = {
+//     name: "Jalol",
+//     surname: "Imomaddinov",
+//     age: 14
+// }
+
+// console.log(user);
+
+// ----------------------------------
+// type point ={x:number, y:number}
+// const point = {
+//     x: 15,
+//     y: 65
+// }
+
+// let task : point = {
+//     x:12,
+//     y:65
+// }
+// console.log(task);
+
+//--------------------------------
+
+// type app = {name:string, downloads:number, description:string}
+// const app = {
+//     name: "Flappy Bird",
+//     downloads: 1456,
+//     description: "Best game in the World!"
+// }
+
+// let newApp: app = {
+//     name: "Flappy Birsasd",
+//     downloads: 141256,
+//     description: "Best game in the World now!"
+// }
+
+// console.log(newApp);
+
+//---------------------------------
+type answerlist = {question:string, answer:number}
+const answerlist = {
+    question: 'Length of lake Niagara?',
+    answer: 123564
+}
+
+let newAnswerList: answerlist = {
+question: 'Length of lake Niagara?',
+answer: 123564
+}
+
+console.log(newAnswerList);
+
+
+
+
+
