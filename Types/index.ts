dc261928938072a19935bcd22b03a355a82107a0
isnaddinovotabek <isnaddinovotabek@gmail.com>
2022-11-22 19:44:28 +0500

    type da object

---
+++ Types/index.ts
@@ -0,0 +1,24 @@
+type Person = {
+    name: string,
+    lastname: string,
+    age: number,
+    email: string,
+    adress: string
+}
+
+const user1: Person = {
+    name: "Otabek",
+    lastname: "Isnaddninov",
+    age: 23,
+    email:"nomail@gmail.com",
+    adress: "Karakalpakistan, Turtkul 72"
+}
+const user2: Person = {
+    name: "Tohir",
+    lastname: "Isnaddninov",
+    age: 21,
+    email:"nomail@gmail.com",
+    adress: "Karakalpakistan, Turtkul 33"
+}
+
+console.log(user1, user2);
