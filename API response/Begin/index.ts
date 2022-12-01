e0b39e90d79ebdf36f107ad11564dc977eb64d1b
isnaddinovotabek <isnaddinovotabek@gmail.com>
2022-12-01 21:55:37 +0500

    Fetch

---
+++ API response/Begin/index.ts
@@ -0,0 +1,22 @@
+import fetch, { Response } from 'node-fetch'
+ const response: Promise<Response> = fetch('https://jsonplaceholder.typicode.com/posts')
+ 
+ type Post = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+ }
+ response .then(res => res.text()) 
+          .then(text => {let posts:Post[] = JSON.parse(text)
+            let filter: Post[] = posts.filter(posts => posts.userId ==4)
+            filter.map(posts => posts.body).join(" ") 
+            console.log();
+            
+             
+          })
+
+
+
+
+ 
