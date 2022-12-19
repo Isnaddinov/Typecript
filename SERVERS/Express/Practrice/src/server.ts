bfc3946bc178bd31c651c2d64b3b3c3747e72572
Otabek Isnaddinov <90689375+Isnaddinov@users.noreply.github.com>
2022-12-19 21:37:12 +0500

    Update server.ts
    Expresda radar to'liq

--- SERVERS/Express/Practrice/src/server.ts
+++ SERVERS/Express/Practrice/src/server.ts
@@ -1,14 +1,13 @@
-import { json } from 'body-parser'
 import express from 'express'
+import { postRadar, getCalc,  } from './calc';
 
 const server = express()
 
 server.use(express.json())
 
-server.post('/radar', )
-server.post('/car', )
-server.post('/fine', )
-server.get('/calc', )
+server.post('/radar',postRadar )
+server.get('/calc',  getCalc)
+
 
 server.listen(9090, () => {
     console.log('Server is running  on host: 9090');
