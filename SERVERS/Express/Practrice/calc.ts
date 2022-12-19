5fd56976760d46f9468e219f1b6cb1f9539e5bd1
Otabek Isnaddinov <90689375+Isnaddinov@users.noreply.github.com>
2022-12-19 21:36:06 +0500

    Update calc.ts
    Expresda radar to'liq

--- SERVERS/Express/Practrice/calc.ts
+++ SERVERS/Express/Practrice/calc.ts
@@ -1,37 +1,38 @@
 import { Request, Response } from "express";
 
-let radar: number[] = []
-let car: number[] = []
-let fine :number[] = []
+
+let radar: Radar[] = [
+
+]
+ 
+
+type Radar = {
+    radar: number,
+    car: number,
+    fine: number
+}
+
 
 export function postRadar(req:Request, res: Response){
-    let rpush = Number(req.body) 
+    let rpush:Radar = req.body
     radar.push(rpush)
-    res.status(200).send("Belgilangan tezlik: " + rpush +'km/h'+ " kiritildi")
+    res.status(200).send("Berilgan ko'rsatkichlar:" + '\n'+ "Belgilangan tezlik: " + radar[0].radar + " km/h"+ '\n'+ "Avtomabil tezliki: " + radar[0].car +" km/h"+ '\n' +"Jarima miqdori har km/h uchun: " + radar[0].fine + " so'm"+'\n' + "Kiritildi")
 }
-export function postCar(req:Request, res: Response){
-    let cpush = Number(req.body) 
-    car.push(cpush)
-    res.status(200).send("Avtomabil tezligi: " + cpush + 'km/h'+ " kirtildi")
-}
-export function postFine(req:Request, res: Response){
-    let fpush = Number(req.body)
-    fine.push(fpush)
-    res.status(200).send("Jarima miqdori: " + fpush +" so'm"+ ' har km/h uchun' + " kiritildi")
-}
+
 export function getCalc(req:Request, res: Response){
-   if(car[0]>radar[0]){
-    const c = car[0]
-    const r = radar[0]
-    const f = fine[0]
+   if(radar[0].car > radar[0].radar){
+    const c = radar[0].car
+    const r = radar[0].radar
+    const f = radar[0].fine
     let h = c-r
     let j = f*h
 
     res.status(200).send('Siz tezlikni oshirdingiz belgilangan tezlik: ' + r + ' km/h' + '\n' + 'Sizning tezligingiz: '+ c +' km/h' +'\n' + 'Belgilangan jarima miqdori har km/h uchun: '+ f +" so'm" +'\n' + 'Jarimangiz: ' + j + " so'm" + '\n' + "Agar 2 oy ichida to'lasangiz 30% skidka")
+    radar = []
 
    } 
-   else if(car[0]<radar[0]){
-    
+   else if(radar[0].car <= radar[0].radar){
+    res.status(200).send("Sizning tezligingiz: " + radar[0].car + " belgilangan tezlikdan oshmagan")
    }
 }
 
